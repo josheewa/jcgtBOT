8086f33dda4b12ce73a0c9465be8c8623c580262
jcgter777 :P <34496783+jcgter777@users.noreply.github.com>
2018-05-31 16:29:22 -0400

    Add some stuff

--- GooGle.py
+++ GooGle.py
@@ -1,1 +1,13 @@
+#!/usr/bin/python3
+import socket
 
+ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server = "irc.efnet.org" # Server
+channel = "#jcgtBOTSMASH" # Channel
+botnick = "GooGle" # Your bots nick
+adminname = "jcgter" #Your IRC nickname. On IRC (and most other places) I go by OrderChaos so that’s what I am using for this example.
+exitcode = "bye " + botnick
+
+ircsock.connect((server, 6697)) # Here we connect to the server using the port 6667
+ircsock.send(bytes("USER "+ botnick +" "+ botnick +" "+ botnick + " " + botnick + "\n", "UTF-8")) #We are basically filling out a form with this line and saying to set all the fields to the bot nickname.
+ircsock.send(bytes("NICK "+ botnick +"\n", "UTF-8")) # assign the nick to the bot
