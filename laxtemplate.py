27c2b5c19b3f080280d93d67c4679dd150b253cf
jcgter777 :P <34496783+jcgter777@users.noreply.github.com>
2018-06-01 17:12:50 -0400

    Create laxtemplate.py

---
+++ laxtemplate.py
@@ -0,0 +1,44 @@
+import socket,time,sys,random
+network = 'network name'
+port = 6667
+channels = ['#BOT'] #Add as many as you want
+nick = 'BOT'
+identify = True
+password = 'Some Password'
+irc = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
+# print "Connecting to "+network+" ..."
+irc.connect ((network, port))
+# print "Changing nick to "+nick+"..."
+irc.send ('NICK '+nick+'\r\n')
+if identify == True:
+        # print "Verifying password..."
+        irc.send("PASS %s\n" % (password))
+# print "Setting login data..."
+# irc.send ('USER '+nick+' '+nick+' '+nick+' :'+nick+' IRC\r\n')
+irc.send ('USER '+nick+' * *  :'+nick+'\r\n')
+time.sleep(1)
+for channel in channels:
+        # print "Joining "+channel+"..."
+        irc.send ('JOIN '+channel+'\r\n')
+        irc.send ('PRIVMSG '+channel+' :'+nick+' Started! Type &help for more\r\n')
+        time.sleep(1)
+# print nick+" bot started."
+while True:
+    data = irc.recv(4096)
+    if data.find('PING') != -1:
+        irc.send('PONG '+data.split()[1]+'\r\n')
+    try:
+        user = data.split("!",1)[0].replace(":","",1)
+        vhost = data.split(" ",1)[0].split("!",1)[1]
+        dType = data.split(" ",1)[1].split(" ",1)[0]
+        chan = data.split(" ",1)[1].split(" ",1)[1].split(" ",1)[0]
+        msg = data.split(" ",1)[1].split(" ",1)[1].split(" ",1)[1].replace(":","",1).replace("\n","",1).replace("\r","",1)
+        lowermsg = msg.lower()
+        nospacemsg = msg.replace (' ','')
+        lowernospacemsg = lowermsg.replace (' ','')
+        if lowermsg.count('!qadd') == 0:
+                if lowermsg == '&help':
+                      irc.send ('PRIVMSG '+chan+' :Please go here to see all commands: https://github.com/LAX18/LAXBOT/wiki/Commands\r\n')
+ 
+    except:
+        pass
